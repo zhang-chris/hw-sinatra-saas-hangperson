130b730432b7275bae05c0fb15ed668a9f637760
Chris Zhang <chris.zhang@berkeley.edu>
2016-02-05 22:37:18 +0000

    Finish part 1

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -10,7 +10,64 @@ class HangpersonGame
   
   def initialize(word)
     @word = word
+	@guesses = ''
+	@wrong_guesses = ''
   end
+
+	attr_accessor:word
+	attr_accessor:guesses
+	attr_accessor:wrong_guesses
+	
+	def check_win_or_lose()
+	  if @wrong_guesses.length >= 7
+	    return :lose
+	  elsif !word_with_guesses().include? '-'
+	    return :win
+	  else
+	    return :play
+	  end
+	end
+	
+
+  def word_with_guesses()
+    w = ''
+    @word.split("").each do |i|
+      if @guesses.include? i
+        w += i
+      else
+        w += '-'
+      end
+    end
+    return w
+  end
+
+
+  def guess_several_letters(letters)
+    letters.split("").each do |i|
+      guess(i)
+    end
+  end
+
+  def guess(letter)
+    if letter !~ /[a-z]/i
+      raise ArgumentError.new("Invalid Argument")
+    end
+    letter = letter.downcase
+    if @word.include? letter
+      if !@guesses.include? letter
+        @guesses += letter
+        return true
+      end
+      return false
+    else
+      if !@wrong_guesses.include? letter
+	      @wrong_guesses += letter
+	      return true
+	    end
+	     return false
+    end
+  end
+
 
   def self.get_random_word
     require 'uri'
--- simple/app.rb
+++
@@ -1,7 +0,0 @@
-require 'sinatra'
-
-class MyApp < Sinatra::Base
-  get '/' do
-    "<!DOCTYPE html><html><head></head><body><h1>Is it working?</h1></body></html>"
-  end
-end
